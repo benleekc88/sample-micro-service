beb43ae6e84bb69d4ce387d844987bac9a8f2bb3
benleekc88 <benleekc888@gmail.com>
2017-01-06 17:50:12 +0800

    Initial commit

---
+++ sample-service/go.sh
@@ -0,0 +1,3 @@
+gradle assemble
+cf push
+cf logs sample-service
---
+++ sample-service/src/main/java/com/manulife/sample/service/Student.java
@@ -0,0 +1,38 @@
+package com.manulife.sample.service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String studentName;
+		
+	public Student() {
+		
+	}
+	
+	public Student(String studentName) {
+		super();
+		this.studentName = studentName;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", studentName=" + studentName + "]";
+	}
+
+}
