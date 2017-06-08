0e6b1cad13f173bb6818d2f7180b5b54a0fe4c81
Ben <ben@BenMac.local>
2017-06-08 09:49:04 +0800

    upgraded to gradle 1.5.3

--- sample-dashboard/src/test/java/com/spring/sample/dashboard/SampleDashboardApplicationTests.java
+++ sample-dashboard/src/test/java/com/spring/sample/dashboard/SampleDashboardApplicationTests.java
@@ -2,13 +2,13 @@ package com.spring.sample.dashboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.spring.sample.dashboard.SampleDashboardApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleDashboardApplication.class)
+@SpringBootTest(classes = SampleDashboardApplication.class)
 public class SampleDashboardApplicationTests {
 
 	@Test
